47dfbcc0e8987cad24cbde6b0a2efeaea23844e2
Denzel1337 <denis-viper2013@ya.ru>
2020-10-13 17:36:29 +0400

    3.3. Разбор первой программы. [#387151]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Denis Mishin");
+        System.out.println("30.10.2000");
+    }
+}
